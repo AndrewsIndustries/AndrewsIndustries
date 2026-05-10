643041ede112f94eaa642170ce5bf03e3bde36fc
AndrewsIndustries <andrewsindustries@gmail.com>
2026-05-09 22:20:14 -0700

    suff
    stuff

--- app.js
+++ app.js
@@ -4,7 +4,3 @@ const localData = await fetch(localUrl).then(res => res.json());
 
 const allArticles = [...worldData.articles, ...localData.articles];
 const tickerText = allArticles.map(a => a.title).join('  •  ');
-
-
-
-
